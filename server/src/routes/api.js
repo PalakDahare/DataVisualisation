7949e48d6fafeaf0792e4ed7b19751583afb0b07
PalakDahare <117660359+PalakDahare@users.noreply.github.com>
2024-08-27 22:00:44 +0530

    Delete server directory

--- server/src/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dataController = require('../controllers/dataController');
-
-
-router.get('/data', dataController.getData);
-router.post('/data', dataController.addData);
-
-module.exports = router;
